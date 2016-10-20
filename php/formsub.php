9ef945988b5497d93b20812065e7435c9e1f1e2f
vidura-supun <vidurasupun@gmail.com>
2016-10-20 16:49:08 +0530

    new

--- php/formsub.php
+++ php/formsub.php
@@ -1,1 +1,12 @@
+<?php
+$fn = _$POST["fn"];
+$ln = _$POST["ln"];
+$ps = crypt(_$POST["ps"]);
+$em = _$POST["em"];
+$fc = _$POST["fc"];
+$si = _$POST["si"];
+$gn = _$POST["gn"];
 
+$conn = ("127.0.0.1", "root", "expect_us", "User");
+$sql = "INSERT INTO users VALUES('vidu', 'supun', 'pass', 'vidu@aa.com', 'SOC', '036', 'M')"
+?>
