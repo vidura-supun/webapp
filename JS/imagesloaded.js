7c1748df9e8b656cdb55a85e08b3056c557962ee
dilanka95 <dilanka.sachin@gmail.com>
2016-10-21 11:01:10 +0530

    Update imagesloaded.js

--- JS/imagesloaded.js
+++ JS/imagesloaded.js
@@ -1,7 +1,4 @@
-/*!
- * imagesLoaded v3.0.2
- * JavaScript is all like "You images are done yet or what?"
- */
+
 
 ( function( window ) {
 
@@ -11,9 +8,7 @@ var $ = window.jQuery;
 var console = window.console;
 var hasConsole = typeof console !== 'undefined';
 
-// -------------------------- helpers -------------------------- //
 
-// extend objects
 function extend( a, b ) {
   for ( var prop in b ) {
     a[ prop ] = b[ prop ];
@@ -26,39 +21,34 @@ function isArray( obj ) {
   return objToString.call( obj ) === '[object Array]';
 }
 
-// turn element or nodeList into an array
+
 function makeArray( obj ) {
   var ary = [];
   if ( isArray( obj ) ) {
-    // use object if already an array
+    
     ary = obj;
   } else if ( typeof obj.length === 'number' ) {
-    // convert nodeList to array
+    
     for ( var i=0, len = obj.length; i < len; i++ ) {
       ary.push( obj[i] );
     }
   } else {
-    // array of single index
+   
     ary.push( obj );
   }
   return ary;
 }
 
-// --------------------------  -------------------------- //
 
 function defineImagesLoaded( EventEmitter, eventie ) {
 
-  /**
-   * @param {Array, Element, NodeList, String} elem
-   * @param {Object or Function} options - if function, use as callback
-   * @param {Function} onAlways - callback function
-   */
+ 
   function ImagesLoaded( elem, options, onAlways ) {
-    // coerce ImagesLoaded() without new, to be new ImagesLoaded()
+   
     if ( !( this instanceof ImagesLoaded ) ) {
       return new ImagesLoaded( elem, options );
     }
-    // use elem as selector string
+  
     if ( typeof elem === 'string' ) {
       elem = document.querySelectorAll( elem );
     }
@@ -79,11 +69,11 @@ function defineImagesLoaded( EventEmitter, eventie ) {
     this.getImages();
 
     if ( $ ) {
-      // add jQuery Deferred object
+     
       this.jqDeferred = new $.Deferred();
     }
 
-    // HACK check async to allow time to bind listeners
+  
     var _this = this;
     setTimeout( function() {
       _this.check();
@@ -97,16 +87,16 @@ function defineImagesLoaded( EventEmitter, eventie ) {
   ImagesLoaded.prototype.getImages = function() {
     this.images = [];
 
-    // filter & find items if we have an item selector
+ 
     for ( var i=0, len = this.elements.length; i < len; i++ ) {
       var elem = this.elements[i];
-      // filter siblings
+      
       if ( elem.nodeName === 'IMG' ) {
         this.addImage( elem );
       }
-      // find children
+      
       var childElems = elem.querySelectorAll('img');
-      // concat childElems to filterFound array
+     
       for ( var j=0, jLen = childElems.length; j < jLen; j++ ) {
         var img = childElems[j];
         this.addImage( img );
@@ -114,9 +104,6 @@ function defineImagesLoaded( EventEmitter, eventie ) {
     }
   };
 
-  /**
-   * @param {Image} img
-   */
   ImagesLoaded.prototype.addImage = function( img ) {
     var loadingImage = new LoadingImage( img );
     this.images.push( loadingImage );
@@ -127,7 +114,7 @@ function defineImagesLoaded( EventEmitter, eventie ) {
     var checkedCount = 0;
     var length = this.images.length;
     this.hasAnyBroken = false;
-    // complete if no images
+  
     if ( !length ) {
       this.complete();
       return;
@@ -143,7 +130,7 @@ function defineImagesLoaded( EventEmitter, eventie ) {
       if ( checkedCount === length ) {
         _this.complete();
       }
-      return true; // bind once
+      return true; 
     }
 
     for ( var i=0; i < length; i++ ) {
@@ -172,8 +159,7 @@ function defineImagesLoaded( EventEmitter, eventie ) {
     }
   };
 
-  // -------------------------- jquery -------------------------- //
-
+ 
   if ( $ ) {
     $.fn.imagesLoaded = function( options, callback ) {
       var instance = new ImagesLoaded( this, options, callback );
@@ -181,8 +167,6 @@ function defineImagesLoaded( EventEmitter, eventie ) {
     };
   }
 
-
-  // --------------------------  -------------------------- //
 
   var cache = {};
 
@@ -193,24 +177,23 @@ function defineImagesLoaded( EventEmitter, eventie ) {
   LoadingImage.prototype = new EventEmitter();
 
   LoadingImage.prototype.check = function() {
-    // first check cached any previous images that have same src
+   
     var cached = cache[ this.img.src ];
     if ( cached ) {
       this.useCached( cached );
       return;
     }
-    // add this to cache
+  
     cache[ this.img.src ] = this;
 
-    // If complete is true and browser supports natural sizes,
-    // try to check for image status manually.
+   
     if ( this.img.complete && this.img.naturalWidth !== undefined ) {
-      // report based on naturalWidth
+      
       this.confirm( this.img.naturalWidth !== 0, 'naturalWidth' );
       return;
     }
 
-    // If none of the checks above matched, simulate loading on detached element.
+  
     var proxyImage = this.proxyImage = new Image();
     eventie.bind( proxyImage, 'load', this );
     eventie.bind( proxyImage, 'error', this );
@@ -224,7 +207,7 @@ function defineImagesLoaded( EventEmitter, eventie ) {
       var _this = this;
       cached.on( 'confirm', function( image ) {
         _this.confirm( image.isLoaded, 'cache emitted confirmed' );
-        return true; // bind once
+        return true;
       });
     }
   };
@@ -235,7 +218,7 @@ function defineImagesLoaded( EventEmitter, eventie ) {
     this.emit( 'confirm', this, message );
   };
 
-  // trigger specified handler for event type
+ 
   LoadingImage.prototype.handleEvent = function( event ) {
     var method = 'on' + event.type;
     if ( this[ method ] ) {
@@ -258,22 +241,22 @@ function defineImagesLoaded( EventEmitter, eventie ) {
     eventie.unbind( this.proxyImage, 'error', this );
   };
 
-  // -----  ----- //
+ 
 
   return ImagesLoaded;
 }
 
-// -------------------------- transport -------------------------- //
+
 
 if ( typeof define === 'function' && define.amd ) {
-  // AMD
+  
   define( [
       'eventEmitter',
       'eventie'
     ],
     defineImagesLoaded );
 } else {
-  // browser global
+  
   window.imagesLoaded = defineImagesLoaded(
     window.EventEmitter,
     window.eventie
